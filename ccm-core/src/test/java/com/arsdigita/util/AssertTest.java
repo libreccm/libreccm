a423f2db846fc1eb7fb51d8df894675463a9b37b
konermann <konermann@8810af33-2d31-482b-a856-94f89814c4df>
2015-07-03 09:27:02 +0000

    CCM NG: improved test coverage of AssertTest.java
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@3517 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/test/java/com/arsdigita/util/AssertTest.java
+++ ccm-core/src/test/java/com/arsdigita/util/AssertTest.java
@@ -35,24 +35,34 @@ import static org.junit.Assert.*;
  */
 @Category(UnitTest.class)
 public class AssertTest {
-    
+
     public AssertTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkFailMessage() {
+        Assert.fail("errormessage");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkFail() {
+        Assert.fail();
     }
 
     /**
@@ -61,12 +71,12 @@ public class AssertTest {
     @Test
     public void assertDisable() {
         Assert.setEnabled(false);
-        
+
         assertThat(Assert.isEnabled(), is(false));
-        
+
         Assert.setEnabled(true);
     }
-    
+
     /**
      * Check if {@link Assert#isTrue(boolean)} fails if condition evaluates to
      * {@code false}.
@@ -75,83 +85,178 @@ public class AssertTest {
     public void checkIfIsTrueFails() {
         Assert.isTrue(false);
     }
-    
+
     /**
-     * Check if {@link Assert#isTrue(boolean, java.lang.String)} fails if 
+     * Check if {@link Assert#isTrue(boolean, java.lang.String)} succeeds if
+     * condition evaluates to {@code true}.
+     */
+    @Test
+    public void checkIsTrue() {
+        Assert.isTrue(true);
+    }
+
+    /**
+     * Check if {@link Assert#isTrue(boolean, java.lang.String)} fails if
      * condition evaluates to {@code false}.
      */
     @Test(expected = AssertionError.class)
     public void checkIfIsTrueWithMessageFails() {
         Assert.isTrue(false, "Expected true");
     }
-    
+
+    /**
+     * Check if {@link Assert#isTrue(boolean, java.lang.String)} succeeds if
+     * condition evaluates to {@code true}.
+     */
+    @Test
+    public void checkIsTrueWithMessage() {
+        Assert.isTrue(true, "Expected true");
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsFalseFails() {
         Assert.isFalse(true);
     }
     
+    @Test
+    public void checkIsFalse() {
+        Assert.isFalse(false);
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsFalseWithMessageFails() {
         Assert.isFalse(true, "Expected false");
     }
     
+    @Test
+    public void checkIsFalseWithMessage() {
+        Assert.isFalse(false, "Expected true");
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfExistsFailsForNull() {
         Assert.exists(null);
     }
-    
+
     @Test
     public void checkExists() {
         Assert.exists("foo");
     }
-    
+
     @Test(expected = AssertionError.class)
     public void checkIfExistsWithClassFailsForNull() {
         Assert.exists(null, Object.class);
     }
-    
+
+    @Test
+    public void checkExistsWithClass() {
+        Assert.exists("foo", Object.class);
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfExistsFailsForNullWithMessage() {
-        Assert.exists(null, "None null object extected");
+        Assert.exists(null, "None null object expected");
     }
-    
+
+    @Test
+    public void checkExistsWithLabel() {
+        Assert.exists("foo", "label");
+    }
+
+    @Test
+    public void checkExistsWithEmptyLabel() {
+        Assert.exists("foo", "");
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsLockedFails() {
         final Lockable unlocked = new LockableImpl();
-        
+
         Assert.isLocked(unlocked);
     }
-    
+
+    @Test
+    public void checkIfIsLockedDoesntThrowError() {
+        Assert.isLocked(null);
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsUnLockedFails() {
         final Lockable locked = new LockableImpl();
         locked.lock();
-        
+
         Assert.isUnlocked(locked);
     }
-    
+
+    @Test
+    public void checkIfIsUnLockedDoesntThrowError() {
+        Assert.isUnlocked(null);
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsEqualFailsForUnequalObjects() {
         Assert.isEqual("foo", "bar");
     }
-    
+
     @Test(expected = AssertionError.class)
     public void checkIfIsEqualFailsForNullAndObject() {
         Assert.isEqual(null, "bar");
     }
-    
+
     @Test(expected = AssertionError.class)
     public void checkIfIsEqualFailsForObjectAndNull() {
         Assert.isEqual("foo", null);
     }
-    
+
+    @Test(expected = AssertionError.class)
+    public void checkIfIsEqualFailsForObjectAndNullWithMessage() {
+        Assert.isEqual("foo", null, "second object is null");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkIfIsEqualFailsForNullAndObjectWithMessage() {
+        Assert.isEqual(null, "foo", "first object is null");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkIfIsEqualFailsForUnequalObjectsWithMessage() {
+        Assert.isEqual("foo", "bar", "unequal ");
+    }
+
     @Test(expected = AssertionError.class)
     public void checkIfIsNotEqualFailsForEqualObjects() {
         Assert.isNotEqual("foo", "foo");
     }
-    
+
     @Test(expected = AssertionError.class)
     public void checkIfIsNotEqualFailsIfBothAreNull() {
         Assert.isNotEqual(null, null);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkIfAssertEqualsFailsIfObjectIsNull() {
+        Assert.assertEquals(null, "bar", "expectedLabel", "actualLabel");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void checkIfAssertEqualsFails() {
+        Assert.assertEquals("foo", "bar", "expectedLabel", "actualLabel");
+    }
+
+    @Test
+    public void checkAssertEqualsIfNotEnabled() {
+        Assert.setEnabled(false);
+        Assert.assertEquals("foo", "bar", "expectedLabel", "actualLabel");
+        Assert.setEnabled(true);
+    }
+
+    @Test
+    public void checkAssertEqualsIfBothNull() {
+        Assert.assertEquals(null, null, "expectedLabel", "actualLabel");
+    }
+
+    @Test
+    public void checkAssertEqualsWithEqualObjects() {
+        Assert.assertEquals("foo", "foo", "expectedLabel", "actualLabel");
     }
 }
