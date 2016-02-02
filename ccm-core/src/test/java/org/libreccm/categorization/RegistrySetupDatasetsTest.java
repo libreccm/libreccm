d2bfec2a1be4a2a81d4dbd532e9a8f6ed716bfc5
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-02-02 08:28:11 +0000

    CCM NG: Setup for registry
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@3835 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-core/src/test/java/org/libreccm/categorization/RegistrySetupDatasetsTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2016 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package org.libreccm.categorization;
+
+import static org.libreccm.testutils.DatasetType.*;
+
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.libreccm.tests.categories.UnitTest;
+import org.libreccm.testutils.DatasetType;
+import org.libreccm.testutils.DatasetsVerifier;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+@RunWith(Parameterized.class)
+@Category(UnitTest.class)
+public class RegistrySetupDatasetsTest extends DatasetsVerifier {
+
+    @Parameterized.Parameters(name = "Dataset {0}")
+    public static Collection<String> data() {
+        return Arrays.asList(new String[]{
+            "/datasets/org/libreccm/categorization/RegistrySetupTest/after-setup.xml"
+        });
+    }
+
+    public RegistrySetupDatasetsTest(final String datasetPath) {
+        super(datasetPath);
+    }
+
+    @Override
+    public String[] getSchemas() {
+        return new String[]{"ccm_core"};
+    }
+
+    @Override
+    public DatasetType getDatasetType() {
+        return FLAT_XML;
+    }
+
+}
