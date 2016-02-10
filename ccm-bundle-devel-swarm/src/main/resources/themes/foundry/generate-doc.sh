e3cc3e8f2042b369bc86abb772ef3cfe9f5d8181
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-02-10 19:56:43 +0000

    CCM NG: Removed obsolete test bundles (Again)
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@3853 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-bundle-devel-swarm/src/main/resources/themes/foundry/generate-doc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-saxon9-transform -s:doc/foundry-documentation.xml  -xsl:start.xsl -o:doc/foundry-documentation.html theme-prefix=`pwd`
--- ccm-test-bundle-wildfly8/src/test/resources-wildfly8-remote-pgsql/scripts/clean_schema.sql
+++
@@ -1,8 +0,0 @@
--- Used by the org.libreccm.core.modules.CcmModulesTest to clean up the
--- schema after the test
-
-DROP SCHEMA IF EXISTS ccm_core CASCADE;
-
-DROP SEQUENCE IF EXISTS hibernate_sequence;
-
-DROP TABLE IF EXISTS schema_version;
