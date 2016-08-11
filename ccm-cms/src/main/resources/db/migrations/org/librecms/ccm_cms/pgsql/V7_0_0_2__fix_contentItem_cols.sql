3c5625b033b5e2d5154d3bae668b2e91419b3c56
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-08-11 16:35:38 +0000

    CCM NG: Increased timeout for wildfly-maven-plugin
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4217 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/src/main/resources/db/migrations/org/librecms/ccm_cms/pgsql/V7_0_0_2__fix_contentItem_cols.sql
+++ ccm-cms/src/main/resources/db/migrations/org/librecms/ccm_cms/pgsql/V7_0_0_2__fix_contentItem_cols.sql
@@ -2,18 +2,12 @@ alter table CCM_CMS.CONTENT_ITEMS
     rename column launchDate to LAUNCH_DATE;
 
 alter table CCM_CMS.CONTENT_ITEMS
-    rename column version to VERSION;
-
-alter table CCM_CMS.CONTENT_ITEMS
     rename column contentType_OBJECT_ID to CONTENT_TYPE_ID;
 
 alter table CCM_CMS.CONTENT_ITEMS_AUD
     rename column launchDate to LAUNCH_DATE;
 
-alter table CCM_CMS.CONTENT_ITEMS
-    rename column version to VERSION;
-
-alter table CCM_CMS.CONTENT_ITEMS
+alter table CCM_CMS.CONTENT_ITEMS_AUD
     rename column contentType_OBJECT_ID to CONTENT_TYPE_ID;
 
 alter table CCM_CMS.CONTENT_TYPES
