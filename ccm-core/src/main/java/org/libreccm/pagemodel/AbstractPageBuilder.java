7d856921ec3c45cd4c5a5a3e4a521c5760c7c42b
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-12-07 18:16:47 +0000

    CCM NG: JavaDoc for PageModel
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4468 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/main/java/org/libreccm/pagemodel/AbstractPageBuilder.java
+++ ccm-core/src/main/java/org/libreccm/pagemodel/AbstractPageBuilder.java
@@ -22,8 +22,12 @@ import java.util.Optional;
 import javax.inject.Inject;
 
 /**
+ * An abstract base class for implementations of the {@link PageBuilder}
+ * interface providing some functionality needed by all implementations of the
+ * {@link PageBuilder} interface.
  *
- * @param <P> Page class
+ * @param <P> Generics variable for the class which represents the page created
+ *            by the {@link PageBuilder}.
  *
  * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
  *
@@ -33,13 +37,24 @@ public abstract class AbstractPageBuilder<P> implements PageBuilder<P> {
     @Inject
     private ComponentBuilderManager componentBuilderManager;
 
+    /**
+     * Build a {@code Page} based on a {@link PageModel}. This implementation
+     * first calls {@link #buildPage()} to create the page object. After that
+     * all {@link ComponentModel}s of the {@link PageModel} are processed and
+     * the component objects created by the {@link ComponentBuilder}s are added
+     * to the page.
+     *
+     * @param pageModel The {@link PageModel\ to process.
+     *
+     * @return A page containing all components from the {@link PageModel}.
+     */
     @Override
     public P buildPage(final PageModel pageModel) {
         final P page = buildPage();
 
         for (final ComponentModel componentModel : pageModel.getComponents()) {
             final Optional<Object> component = buildComponent(
-                    componentModel, componentModel.getClass());
+                componentModel, componentModel.getClass());
             if (component.isPresent()) {
                 addComponent(page, component);
             }
@@ -48,15 +63,25 @@ public abstract class AbstractPageBuilder<P> implements PageBuilder<P> {
         return page;
     }
 
+    /**
+     * Helper method for building the components.
+     *
+     * @param <M>                 Generics variable for the type the component
+     *                            created.
+     * @param componentModel      The {@link ComponentModel} to process.
+     * @param componentModelClass The class of the {@link ComponentModel}.
+     *
+     * @return The components described by the {@code componentModel}.
+     */
     protected <M extends ComponentModel> Optional<Object> buildComponent(
-            final ComponentModel componentModel,
-            final Class<M> componentModelClass) {
+        final ComponentModel componentModel,
+        final Class<M> componentModelClass) {
 
         componentBuilderManager.findComponentBuilder(componentModel.getClass(),
                                                      getType());
 
         final Optional<ComponentBuilder<M, ?>> builder = componentBuilderManager
-                .findComponentBuilder(componentModelClass, getType());
+            .findComponentBuilder(componentModelClass, getType());
 
         if (builder.isPresent()) {
             return Optional.of(builder.get().buildComponent((M) componentModel));
@@ -65,7 +90,22 @@ public abstract class AbstractPageBuilder<P> implements PageBuilder<P> {
         }
     }
 
+    /**
+     * Abstract method returning the type (view technology) for which the
+     * {@link PageBuilder} processes {@link PageModel}s.
+     *
+     * @return
+     */
     protected abstract String getType();
 
+    /**
+     * A helper method for adding components to the page. How this is done
+     * depends on the view technology, therefore this method must be implemented
+     * by the implementations of this abstract class.
+     *
+     * @param page The page to which the component is added.
+     * @param component The component to add to the page.
+     */
     protected abstract void addComponent(P page, Object component);
+
 }
