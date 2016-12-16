b01b9fb35a98676832fbbaa7350d356df65baaf2
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-12-16 16:41:16 +0000

    CCM NG: - Some preparation for the editor UI for PageModel - SimpleItemResolver and MultilingualItemResolver ported to new API (still disabled because of circular dependencies in UI classes)
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4486 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-cms/src/main/java/org/librecms/contentsection/ContentItemRepository.java
+++ ccm-cms/src/main/java/org/librecms/contentsection/ContentItemRepository.java
@@ -40,10 +40,13 @@ import org.libreccm.workflow.Workflow;
  */
 @RequestScoped
 public class ContentItemRepository
-        extends AbstractAuditedEntityRepository<Long, ContentItem> {
+    extends AbstractAuditedEntityRepository<Long, ContentItem> {
 
     @Inject
     private CcmObjectRepository ccmObjectRepo;
+
+    @Inject
+    private FolderRepository folderRepo;
 
     @Override
     public Long getEntityId(final ContentItem item) {
@@ -59,7 +62,7 @@ public class ContentItemRepository
     public boolean isNew(final ContentItem item) {
         return ccmObjectRepo.isNew(item);
     }
-    
+
     @Override
     public void initNewEntity(final ContentItem item) {
         final String uuid = UUID.randomUUID().toString();
@@ -75,7 +78,7 @@ public class ContentItemRepository
      * @param itemId The id of item to retrieve.
      *
      * @return The content item identified by the provided {@code itemId} or
-     * nothing if there is such content item.
+     *         nothing if there is such content item.
      */
     public Optional<ContentItem> findById(final long itemId) {
         final Optional<CcmObject> result = ccmObjectRepo.findObjectById(itemId);
@@ -89,13 +92,13 @@ public class ContentItemRepository
     /**
      * Finds a content item by its ID and ensures that is a the requested type.
      *
-     * @param <T> The type of the content item.
+     * @param <T>    The type of the content item.
      * @param itemId The id of item to retrieve.
-     * @param type The type of the content item.
+     * @param type   The type of the content item.
      *
      * @return The content item identified by the provided id or an empty
-     * {@link Optional} if there is no such item or if it is not of the
-     * requested type.
+     *         {@link Optional} if there is no such item or if it is not of the
+     *         requested type.
      */
     @SuppressWarnings("unchecked")
     public <T extends ContentItem> Optional<T> findById(final long itemId,
@@ -114,7 +117,7 @@ public class ContentItemRepository
      * @param uuid The id of item to retrieve.
      *
      * @return The content item identified by the provided {@code uuid} or
-     * nothing if there is such content item.
+     *         nothing if there is such content item.
      */
     public Optional<ContentItem> findByUuid(final String uuid) {
         final Optional<CcmObject> result = ccmObjectRepo.findObjectByUuid(uuid);
@@ -129,21 +132,21 @@ public class ContentItemRepository
      * Finds a content item by its UUID and ensures that is a the requested
      * type.
      *
-     * @param <T> The type of the content item.
+     * @param <T>  The type of the content item.
      * @param uuid The UUID of item to retrieve.
      * @param type The type of the content item.
      *
      * @return The content item identified by the provided UUID or an empty
-     * {@link Optional} if there is no such item or if it is not of the
-     * requested type.
+     *         {@link Optional} if there is no such item or if it is not of the
+     *         requested type.
      */
     @SuppressWarnings("unchecked")
     public <T extends ContentItem> Optional<T> findByUuid(final String uuid,
                                                           final Class<T> type) {
         final Optional<CcmObject> result = ccmObjectRepo.findObjectByUuid(uuid);
 
-        if (result.isPresent() 
-            && result.get().getClass().isAssignableFrom(type)) {
+        if (result.isPresent()
+                && result.get().getClass().isAssignableFrom(type)) {
             return Optional.of((T) result.get());
         } else {
             return Optional.empty();
@@ -153,7 +156,7 @@ public class ContentItemRepository
     /**
      * Finds all content items of a specific type.
      *
-     * @param <T> The type of the items.
+     * @param <T>  The type of the items.
      * @param type The type of the items.
      *
      * @return A list of all content items of the requested type.
@@ -161,7 +164,7 @@ public class ContentItemRepository
     @SuppressWarnings("unchecked")
     public <T extends ContentItem> List<T> findByType(final Class<T> type) {
         final TypedQuery<ContentItem> query = getEntityManager()
-                .createNamedQuery("ContentItem.findByType", ContentItem.class);
+            .createNamedQuery("ContentItem.findByType", ContentItem.class);
         query.setParameter("type", type);
 
         return (List<T>) query.getResultList();
@@ -176,10 +179,10 @@ public class ContentItemRepository
      */
     public List<ContentItem> findByFolder(final Category folder) {
         final TypedQuery<ContentItem> query = getEntityManager()
-                .createNamedQuery("ContentItem.findByFolder",
-                                  ContentItem.class);
+            .createNamedQuery("ContentItem.findByFolder",
+                              ContentItem.class);
         query.setParameter("folder", folder);
-        
+
         return query.getResultList();
     }
 
@@ -187,14 +190,30 @@ public class ContentItemRepository
      * Counts the items in a folder/category.
      *
      * @param folder The folder/category
+     *
      * @return The number of content items in the category/folder.
      */
     public long countItemsInFolder(final Category folder) {
         final TypedQuery<Long> query = getEntityManager()
-                .createNamedQuery("ContentItem.countItemsInFolder", Long.class);
+            .createNamedQuery("ContentItem.countItemsInFolder", Long.class);
         query.setParameter("folder", folder);
 
         return query.getSingleResult();
+    }
+
+    public Optional<ContentItem> findByNameInFolder(final Category folder,
+                                                    final String name) {
+        final TypedQuery<ContentItem> query = getEntityManager()
+            .createNamedQuery("ContentItem.findByNameInFolder",
+                              ContentItem.class);
+        query.setParameter("folder", folder);
+        query.setParameter("name", name);
+
+        try {
+            return Optional.of(query.getSingleResult());
+        } catch (NoResultException ex) {
+            return Optional.empty();
+        }
     }
 
     /**
@@ -202,11 +221,12 @@ public class ContentItemRepository
      *
      * @param folder
      * @param name
+     *
      * @return
      */
     public long countByNameInFolder(final Category folder, final String name) {
         final TypedQuery<Long> query = getEntityManager().createNamedQuery(
-                "ContentItem.countByNameInFolder", Long.class);
+            "ContentItem.countByNameInFolder", Long.class);
         query.setParameter("folder", folder);
         query.setParameter("name", name);
 
@@ -219,14 +239,15 @@ public class ContentItemRepository
      * pattern.
      *
      * @param folder The folder/category whose items are filtered.
-     * @param name The name pattern to use.
+     * @param name   The name pattern to use.
+     *
      * @return A list with all items in the folder matching the provided filter.
      */
     public List<ContentItem> filterByFolderAndName(final Category folder,
                                                    final String name) {
         final TypedQuery<ContentItem> query = getEntityManager()
-                .createNamedQuery("ContentItem.filterByFolderAndName",
-                                  ContentItem.class);
+            .createNamedQuery("ContentItem.filterByFolderAndName",
+                              ContentItem.class);
         query.setParameter("folder", folder);
         query.setParameter("name", name);
 
@@ -238,15 +259,16 @@ public class ContentItemRepository
      * starts with the provided pattern.
      *
      * @param folder The folder/category to use.
-     * @param name The name pattern to use.
+     * @param name   The name pattern to use.
+     *
      * @return The number of items in the folder/category which match the
-     * provided pattern.
+     *         provided pattern.
      */
     public long countFilterByFolderAndName(final Category folder,
                                            final String name) {
         final TypedQuery<Long> query = getEntityManager()
-                .createNamedQuery("ContentItem.countFilterByFolderAndName",
-                                  Long.class);
+            .createNamedQuery("ContentItem.countFilterByFolderAndName",
+                              Long.class);
         query.setParameter("folder", folder);
         query.setParameter("name", name);
 
@@ -255,15 +277,74 @@ public class ContentItemRepository
 
     public Optional<ContentItem> findItemWithWorkflow(final Workflow workflow) {
         final TypedQuery<ContentItem> query = getEntityManager()
-                .createNamedQuery("ContentItem.findItemWithWorkflow", 
-                                  ContentItem.class);
+            .createNamedQuery("ContentItem.findItemWithWorkflow",
+                              ContentItem.class);
         query.setParameter("workflow", workflow);
-        
+
         try {
             return Optional.of(query.getSingleResult());
-        } catch(NoResultException ex) {
+        } catch (NoResultException ex) {
             return Optional.empty();
         }
     }
-    
+
+    /**
+     * Finds a {@link ContentItem} by its path inside a {@link ContentSection}.
+     *
+     * @param path The path of the item <strong>including</strong> the content
+     *             section, separated from the rest of the path by a
+     *             <code>:</code>.
+     *
+     * @return An {@link Optional} containing the content item identified by the
+     *         provided path or an empty {@code Optional} if there is no such
+     *         item.
+     */
+    public Optional<ContentItem> findByPath(final String path) {
+        //The last token is the name of the item itself. Remove this part an get 
+        //the folder containing the item using the FolderRepository.
+        final String normalizedPath = PathUtil.normalizePath(path);
+        final int lastTokenStart = normalizedPath.lastIndexOf('/');
+        final String folderPath = normalizedPath.substring(0, lastTokenStart);
+        final String itemName = normalizedPath.substring(lastTokenStart + 1);
+
+        final Optional<Folder> folder = folderRepo.findByPath(
+            folderPath, FolderType.DOCUMENTS_FOLDER);
+
+        if (folder.isPresent()) {
+            return findByNameInFolder(folder.get(), itemName);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Finds a {@link ContentItem} by its path inside the provided
+     * {@link ContentSection}.
+     *
+     * @param section The section to which the item belongs.
+     * @param path    The path of the item inside this content section.
+     *
+     * @return An {@link Optional} containing the content item identified by the
+     *         provided path or an empty {@code Optional} if there is no such
+     *         item.
+     */
+    public Optional<ContentItem> findByPath(final ContentSection section,
+                                            final String path) {
+        //The last token is the name of the item itself. Remove this part an get 
+        //the folder containing the item using the FolderRepository.
+        final String normalizedPath = PathUtil.normalizePath(path);
+        final int lastTokenStart = normalizedPath.lastIndexOf('/');
+        final String folderPath = normalizedPath.substring(0, lastTokenStart);
+        final String itemName = normalizedPath.substring(lastTokenStart + 1);
+
+        final Optional<Folder> folder = folderRepo.findByPath(
+            section, folderPath, FolderType.DOCUMENTS_FOLDER);
+
+        if (folder.isPresent()) {
+            return findByNameInFolder(folder.get(), itemName);
+        } else {
+            return Optional.empty();
+        }
+    }
+
 }
---
+++ ccm-core/src/main/java/org/libreccm/pagemodel/ui/PageModelTreeModelBuilder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package org.libreccm.pagemodel.ui;
+
+import com.arsdigita.bebop.PageState;
+import com.arsdigita.bebop.Tree;
+import com.arsdigita.bebop.tree.TreeModel;
+import com.arsdigita.bebop.tree.TreeModelBuilder;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+class PageModelTreeModelBuilder implements TreeModelBuilder {
+
+    @Override
+    public TreeModel makeModel(final Tree tree, final PageState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void lock() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isLocked() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
