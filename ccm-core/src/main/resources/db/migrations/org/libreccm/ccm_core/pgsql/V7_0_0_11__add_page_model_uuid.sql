9ef1169553858725994a1545d8ea29243774a984
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2016-12-19 19:13:24 +0000

    CCM NG: DB Migration fix
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4492 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/main/resources/db/migrations/org/libreccm/ccm_core/pgsql/V7_0_0_11__add_page_model_uuid.sql
+++ ccm-core/src/main/resources/db/migrations/org/libreccm/ccm_core/pgsql/V7_0_0_11__add_page_model_uuid.sql
@@ -1,3 +1,5 @@
 alter table CCM_CORE.PAGE_MODELS
     add column MODEL_UUID varchar(255) not null;
 
+alter table CCM_CORE.PAGE_MODEL_COMPONENT_MODELS
+    add column MODEL_UUID varchar(255) not null;
