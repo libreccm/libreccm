32b4092d65de9d93c03b7b1430caaf15f3393073
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-01-21 07:40:26 +0000

    CCM NG/ccm-shortcuts: Tests pass again
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4529 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: ad510104661261b07536764eb6bd765f7e708c37

--- ccm-shortcuts/src/test/java/org/libreccm/shortcuts/ShortcutRepositoryTest.java
+++ ccm-shortcuts/src/test/java/org/libreccm/shortcuts/ShortcutRepositoryTest.java
@@ -100,6 +100,7 @@ public class ShortcutRepositoryTest {
                 .getPackage())
             .addPackage(org.libreccm.l10n.LocalizedString.class
                 .getPackage())
+            .addClass(org.libreccm.portation.Portable.class)
             .addPackage(org.libreccm.security.Permission.class.getPackage())
             .addPackage(org.libreccm.web.CcmApplication.class.getPackage())
             .addPackage(org.libreccm.workflow.Workflow.class.getPackage())
