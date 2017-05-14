da256e4c046af7bda2f32c7b3fa152759ea1446e
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-05-14 17:46:06 +0000

    CCM NG/ccm-core: More work on the Vaadin prototype
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4735 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: f5022840148915c27fe9c797827c66245c6f7058

--- ccm-core/src/main/java/org/libreccm/admin/ui/usersgroupsroles/UserEditor.java
+++ ccm-core/src/main/java/org/libreccm/admin/ui/usersgroupsroles/UserEditor.java
@@ -20,13 +20,14 @@ package org.libreccm.admin.ui.usersgroupsroles;
 
 import com.arsdigita.ui.admin.AdminUiConstants;
 
+import com.vaadin.data.HasValue;
 import com.vaadin.data.provider.AbstractDataProvider;
 import com.vaadin.data.provider.Query;
+import com.vaadin.server.UserError;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.ItemCaptionGenerator;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.RadioButtonGroup;
@@ -34,15 +35,23 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.libreccm.admin.ui.ConfirmDiscardDialog;
+import org.libreccm.cdi.utils.CdiUtil;
+import org.libreccm.core.EmailAddress;
 import org.libreccm.core.UnexpectedErrorException;
+import org.libreccm.security.ChallengeManager;
 import org.libreccm.security.User;
 import org.libreccm.security.UserManager;
 import org.libreccm.security.UserRepository;
 
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import javax.mail.MessagingException;
 
 /**
  *
@@ -52,16 +61,22 @@ public class UserEditor extends Window {
 
     private static final long serialVersionUID = 7024424532574023431L;
 
+    private static final Logger LOGGER = LogManager.getLogger(UserEditor.class);
+
     private enum PasswordOptions {
 
+        DO_NOTHING,
         GENERATE_AND_SEND,
-        SET
+        SET,
 
     }
 
+    private final UsersGroupsRoles usersGroupsRoles;
     private final User user;
     private final UserRepository userRepo;
     private final UserManager userManager;
+
+    private boolean dataHasChanged = false;
 
     private TextField userName;
     private TextField familyName;
@@ -73,11 +88,13 @@ public class UserEditor extends Window {
     private CheckBox passwordResetRequired;
     private CheckBox banned;
 
-    public UserEditor(final UserRepository userRepo,
+    public UserEditor(final UsersGroupsRoles usersGroupsRoles,
+                      final UserRepository userRepo,
                       final UserManager userManager) {
-        
+
         super("Create new user");
-        
+
+        this.usersGroupsRoles = usersGroupsRoles;
         user = null;
         this.userRepo = userRepo;
         this.userManager = userManager;
@@ -86,12 +103,14 @@ public class UserEditor extends Window {
     }
 
     public UserEditor(final User user,
+                      final UsersGroupsRoles usersGroupsRoles,
                       final UserRepository userRepo,
                       final UserManager userManager) {
 
         super(String.format("Edit user %s", user.getName()));
-        
+
         this.user = user;
+        this.usersGroupsRoles = usersGroupsRoles;
         this.userRepo = userRepo;
         this.userManager = userManager;
 
@@ -103,20 +122,27 @@ public class UserEditor extends Window {
         final ResourceBundle bundle = ResourceBundle
             .getBundle(AdminUiConstants.ADMIN_BUNDLE,
                        UI.getCurrent().getLocale());
-        
+
+        final DataHasChangedListener dataHasChangedListener
+                                         = new DataHasChangedListener();
+
         userName = new TextField(bundle
             .getString("ui.admin.user_edit.username.label"));
         userName.setRequiredIndicatorVisible(true);
+        userName.addValueChangeListener(dataHasChangedListener);
 
         familyName = new TextField(bundle
             .getString("ui.admin.user_edit.familyname.label"));
+        familyName.addValueChangeListener(dataHasChangedListener);
 
         givenName = new TextField(bundle
             .getString("ui.admin.user_edit.givenname.label"));
+        givenName.addValueChangeListener(dataHasChangedListener);
 
         emailAddress = new TextField(bundle
             .getString("ui.admin.user_edit.emailAddress.label"));
         emailAddress.setRequiredIndicatorVisible(true);
+        givenName.addValueChangeListener(dataHasChangedListener);
 
         passwordOptions = new RadioButtonGroup<PasswordOptions>(
             bundle.getString("ui.admin.user_edit.password_options.label"),
@@ -131,13 +157,23 @@ public class UserEditor extends Window {
 
             @Override
             public int size(final Query<PasswordOptions, String> query) {
-                return PasswordOptions.values().length;
+                if (user == null) {
+                    return PasswordOptions.values().length - 1;
+                } else {
+                    return PasswordOptions.values().length;
+                }
             }
 
             @Override
             public Stream<PasswordOptions> fetch(
                 final Query<PasswordOptions, String> query) {
-                return Arrays.stream(PasswordOptions.values());
+                if (user == null) {
+                    return Arrays
+                        .stream(PasswordOptions.values())
+                        .filter(option -> option != PasswordOptions.DO_NOTHING);
+                } else {
+                    return Arrays.stream(PasswordOptions.values());
+                }
             }
 
         });
@@ -150,6 +186,9 @@ public class UserEditor extends Window {
                     case SET:
                         return bundle.getString(
                             "ui.admin.user_edit.password_options.set");
+                    case DO_NOTHING:
+                        return bundle.getString(
+                            "ui.admin.user_edit.password_options.do_nothing");
                     default:
                         throw new UnexpectedErrorException(String.format(
                             "Unexpected value '%s' for password options.",
@@ -160,6 +199,7 @@ public class UserEditor extends Window {
         password = new PasswordField(bundle
             .getString("ui.admin.user_edit.password.label"));
         password.setRequiredIndicatorVisible(true);
+        password.addValueChangeListener(dataHasChangedListener);
 
         passwordConfirmation = new PasswordField(bundle
             .getString("ui.admin.user_set_password_confirm.label"));
@@ -179,6 +219,12 @@ public class UserEditor extends Window {
                     passwordConfirmation.setEnabled(true);
                     passwordConfirmation.setVisible(true);
                     break;
+                case DO_NOTHING:
+                    password.setEnabled(false);
+                    password.setVisible(false);
+                    passwordConfirmation.setEnabled(false);
+                    passwordConfirmation.setVisible(false);
+                    break;
                 default:
                     throw new UnexpectedErrorException(String.format(
                         "Unexpected value '%s' for password options.",
@@ -188,11 +234,16 @@ public class UserEditor extends Window {
 
         passwordOptions.setValue(PasswordOptions.GENERATE_AND_SEND);
 
+        final CheckBoxHasChangedListener checkBoxHasChangedListener
+                                             = new CheckBoxHasChangedListener();
+
         passwordResetRequired = new CheckBox(bundle
             .getString("ui.admin.user_edit.password_reset_required.label"));
+        passwordResetRequired.addValueChangeListener(checkBoxHasChangedListener);
 
         banned = new CheckBox(bundle
             .getString("ui.admin.user_edit.banned.label"));
+        banned.addValueChangeListener(checkBoxHasChangedListener);
 
         if (user == null) {
             banned.setVisible(false);
@@ -206,6 +257,7 @@ public class UserEditor extends Window {
         } else {
             submit.setCaption(bundle.getString("ui.admin.save"));
         }
+        submit.addClickListener(event -> saveUser());
 
         final Button cancel = new Button(bundle.getString("ui.admin.cancel"));
         cancel.addClickListener(event -> close());
@@ -234,16 +286,182 @@ public class UserEditor extends Window {
         }
 
         setContent(panel);
+
+        if (user != null) {
+            userName.setValue(user.getName());
+            givenName.setValue(user.getGivenName());
+            familyName.setValue(user.getFamilyName());
+            emailAddress.setValue(user.getPrimaryEmailAddress().getAddress());
+            passwordResetRequired.setValue(user.isPasswordResetRequired());
+            banned.setValue(user.isBanned());
+            passwordOptions.setValue(PasswordOptions.DO_NOTHING);
+        }
     }
-    
+
     @Override
     public void close() {
-        
-        final ConfirmDiscardDialog dialog = new ConfirmDiscardDialog(
+
+        if (dataHasChanged) {
+            final ConfirmDiscardDialog dialog = new ConfirmDiscardDialog(
                 this, "Are you sure to discard the changes made this user?");
             dialog.setModal(true);
             UI.getCurrent().addWindow(dialog);
-        
+        } else {
+            super.close();
+        }
+    }
+
+    protected void saveUser() {
+
+        final ResourceBundle bundle = ResourceBundle
+            .getBundle(AdminUiConstants.ADMIN_BUNDLE,
+                       UI.getCurrent().getLocale());
+
+        boolean valid = true;
+
+        if (userName.getValue() == null
+                || userName.getValue().trim().isEmpty()) {
+            userName.setComponentError(new UserError(bundle
+                .getString("ui.admin.user_edit.username.error.not_empty")));
+            valid = false;
+        }
+        if (emailAddress.getValue() == null
+                || emailAddress.getValue().trim().isEmpty()) {
+            emailAddress.setComponentError(new UserError(bundle
+                .getString("ui.admin.user.email_form.address.not_empty")));
+            valid = false;
+        }
+
+        if ((user == null || !user.getName().equals(userName.getValue()))
+                && userRepo.isNameInUse(userName.getValue())) {
+
+            userName.setComponentError(new UserError(bundle
+                .getString("ui.admin.new_user_form.error"
+                               + ".username_already_in_use")));
+            valid = false;
+        } else {
+            userName.setComponentError(null);
+        }
+
+        if ((user == null
+             || !user.getPrimaryEmailAddress().getAddress().equals(
+             emailAddress.getValue()))
+                && userRepo.isEmailAddressInUse(emailAddress.getValue())) {
+
+            emailAddress.setComponentError(new UserError(bundle
+                .getString("ui.admin.new_user_form.error.email_already_in_use")));
+            valid = false;
+        } else {
+            if (!Pattern.matches(
+                "^[^@<>\"\\t ]+@[^@<>\".\\t]+([.][^@<>\".\\n ]+)+$",
+                emailAddress.getValue().trim())) {
+                emailAddress.setComponentError(new UserError(bundle
+                    .getString("ui.admin.user_form.email_malformed")));
+                valid = false;
+            } else {
+                emailAddress.setComponentError(null);
+            }
+        }
+
+        if (passwordOptions.getValue() == PasswordOptions.SET) {
+            if (password.getValue() == null
+                    || password.getValue().trim().isEmpty()) {
+                password.setComponentError(new UserError(bundle
+                    .getString(
+                        "ui.admin.set_password.new_password.error.not_empty")));
+            }
+
+            if (!password.getValue().equals(passwordConfirmation.getValue())) {
+                passwordConfirmation.setComponentError(new UserError(bundle
+                    .getString("ui.admin.user_set_password.error.do_not_match")));
+            }
+        }
+
+        if (!valid) {
+            return;
+        }
+
+        final User currentUser;
+        if (user == null) {
+
+            final User newUser = userManager.createUser(givenName.getValue(),
+                                                        familyName.getValue(),
+                                                        userName.getValue(),
+                                                        emailAddress.getValue(),
+                                                        passwordConfirmation
+                                                            .getValue());
+
+            newUser.setPasswordResetRequired(passwordResetRequired.getValue());
+            newUser.setBanned(banned.getValue());
+            currentUser = newUser;
+
+            userRepo.save(newUser);
+        } else {
+            user.setName(userName.getValue().trim());
+            user.setGivenName(givenName.getValue().trim());
+            user.setFamilyName(familyName.getValue().trim());
+            final EmailAddress email = user.getPrimaryEmailAddress();
+            if (!email.getAddress().equals(emailAddress.getValue())) {
+                email.setAddress(emailAddress.getValue());
+            }
+            user.setPasswordResetRequired(passwordResetRequired.getValue());
+            user.setBanned(banned.getValue());
+
+            userRepo.save(user);
+            currentUser = user;
+        }
+
+        switch (passwordOptions.getValue()) {
+            case GENERATE_AND_SEND: {
+                userManager.updatePassword(currentUser, null);
+                final ChallengeManager challengeManager = CdiUtil
+                    .createCdiUtil()
+                    .findBean(ChallengeManager.class);
+                try {
+                    challengeManager.sendPasswordRecover(currentUser);
+                } catch (MessagingException ex) {
+                    setComponentError(new UserError(bundle
+                        .getString("ui.admin.user_form"
+                                       + ".failed_to_send_password_challenge")));
+                    LOGGER.error(
+                        "Failed to send password challenge.",
+                        ex);
+                }
+                break;
+            }
+            case SET:
+                userManager.updatePassword(currentUser,
+                                           passwordConfirmation.getValue());
+                break;
+        }
+
+        dataHasChanged = false;
+        usersGroupsRoles.refreshUsers();
+        close();
+    }
+
+    private class DataHasChangedListener implements
+        HasValue.ValueChangeListener<String> {
+
+        private static final long serialVersionUID = -4698658552890778877L;
+
+        @Override
+        public void valueChange(final HasValue.ValueChangeEvent<String> event) {
+            dataHasChanged = true;
+        }
+
+    }
+
+    private class CheckBoxHasChangedListener implements
+        HasValue.ValueChangeListener<Boolean> {
+
+        private static final long serialVersionUID = 1986372149566327203L;
+
+        @Override
+        public void valueChange(final HasValue.ValueChangeEvent<Boolean> event) {
+            dataHasChanged = true;
+        }
+
     }
 
 }
