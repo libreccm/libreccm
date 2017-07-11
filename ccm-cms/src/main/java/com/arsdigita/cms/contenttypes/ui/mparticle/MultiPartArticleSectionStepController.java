36f19d6f53dbd3d75ede981ad8f30c9e8ed8c687
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-07-11 15:18:33 +0000

    CCM NG/ccm-cms: First part of authoring step for managing the sections of a multi part article
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@4863 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: 6a4c44305c72f2168384319e9c27e4ac4ccdeac7

---
+++ ccm-cms/src/main/java/com/arsdigita/cms/contenttypes/ui/mparticle/MultiPartArticleSectionStepController.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package com.arsdigita.cms.contenttypes.ui.mparticle;
+
+import org.librecms.contentsection.ContentItemRepository;
+import org.librecms.contenttypes.MultiPartArticle;
+import org.librecms.contenttypes.MultiPartArticleSection;
+import org.librecms.contenttypes.MultiPartArticleSectionManager;
+import org.librecms.contenttypes.MultiPartArticleSectionRepository;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+@RequestScoped
+public class MultiPartArticleSectionStepController {
+
+    @Inject
+    private ContentItemRepository itemRepo;
+
+    @Inject
+    private MultiPartArticleSectionRepository sectionRepo;
+
+    @Inject
+    private MultiPartArticleSectionManager sectionManager;
+
+    public void moveToFirst(final MultiPartArticle article,
+                            final MultiPartArticleSection section) {
+
+        final MultiPartArticle theArticle = itemRepo
+            .findById(article.getObjectId(),
+                      MultiPartArticle.class)
+            .orElseThrow(() -> new IllegalArgumentException(String.format(
+            "No MultiPartArticle with ID %d in the database.",
+            article.getObjectId())));
+
+        final MultiPartArticleSection theSection = sectionRepo
+        .findById(section.getSectionId())
+        .orElseThrow(() -> new IllegalArgumentException(String.format(
+            "No MultiPartArticleSection with ID %d in the database.",
+            section.getSectionId())));
+        
+        sectionManager.moveToFirst(article, section);
+    }
+
+}
