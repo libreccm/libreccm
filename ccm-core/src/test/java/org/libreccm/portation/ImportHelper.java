4442c13edb4a813342e09379c206f18cbed4073e
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-09-27 19:57:37 +0000

    CCM NG: Fixed some compilion failures
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5016 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/test/java/org/libreccm/portation/ImportHelper.java
+++ ccm-core/src/test/java/org/libreccm/portation/ImportHelper.java
@@ -18,13 +18,39 @@
  */
 package org.libreccm.portation;
 
-import org.libreccm.categorization.*;
+import org.libreccm.categorization.Categorization;
+import org.libreccm.categorization.CategorizationMarshaller;
+import org.libreccm.categorization.Category;
+import org.libreccm.categorization.CategoryMarshaller;
+import org.libreccm.categorization.Domain;
+import org.libreccm.categorization.DomainMarshaller;
+import org.libreccm.categorization.DomainOwnership;
+import org.libreccm.categorization.DomainOwnershipMarshaller;
 import org.libreccm.core.ResourceType;
 import org.libreccm.core.ResourceTypeMarshaller;
-import org.libreccm.security.*;
+import org.libreccm.security.Group;
+import org.libreccm.security.GroupMarshaller;
+import org.libreccm.security.GroupMembership;
+import org.libreccm.security.GroupMembershipMarshaller;
+import org.libreccm.security.Permission;
+import org.libreccm.security.PermissionMarshaller;
+import org.libreccm.security.Role;
+import org.libreccm.security.RoleMarshaller;
+import org.libreccm.security.RoleMembership;
+import org.libreccm.security.RoleMembershipMarshaller;
+import org.libreccm.security.User;
+import org.libreccm.security.UserMarshaller;
+
 import org.libreccm.web.CcmApplication;
 import org.libreccm.web.ApplicationMarshaller;
-import org.libreccm.workflow.*;
+import org.libreccm.workflow.AssignableTask;
+import org.libreccm.workflow.AssignableTaskMarshaller;
+import org.libreccm.workflow.TaskAssignment;
+import org.libreccm.workflow.TaskAssignmentMarshaller;
+import org.libreccm.workflow.TaskComment;
+import org.libreccm.workflow.TaskCommentMarshaller;
+import org.libreccm.workflow.Workflow;
+import org.libreccm.workflow.WorkflowMarshaller;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -47,34 +73,47 @@ class ImportHelper {
     @Inject
     @Marshals(User.class)
     private UserMarshaller userMarshaller;
+    
     @Inject
     @Marshals(Group.class)
     private GroupMarshaller groupMarshaller;
+    
     @Inject
     @Marshals(GroupMembership.class)
     private GroupMembershipMarshaller groupMembershipMarshaller;
+    
     @Inject
     @Marshals(Role.class)
     private RoleMarshaller roleMarshaller;
+    
     @Inject
     @Marshals(RoleMembership.class)
     private RoleMembershipMarshaller roleMembershipMarshaller;
 
     @Inject
+    @Marshals(Permission.class)
+    private PermissionMarshaller permissionMarshaller;
+    
+    @Inject
     @Marshals(Category.class)
     private CategoryMarshaller categoryMarshaller;
+    
     @Inject
     @Marshals(Categorization.class)
     private CategorizationMarshaller categorizationMarshaller;
+    
     @Inject
     @Marshals(ResourceType.class)
     private ResourceTypeMarshaller resourceTypeMarshaller;
+    
     @Inject
     @Marshals(CcmApplication.class)
     private ApplicationMarshaller applicationMarshaller;
+    
     @Inject
     @Marshals(Domain.class)
     private DomainMarshaller domainMarshaller;
+    
     @Inject
     @Marshals(DomainOwnership.class)
     private DomainOwnershipMarshaller domainOwnershipMarshaller;
@@ -82,12 +121,15 @@ class ImportHelper {
     @Inject
     @Marshals(Workflow.class)
     private WorkflowMarshaller workflowMarshaller;
+    
     @Inject
     @Marshals(TaskComment.class)
     private TaskCommentMarshaller taskCommentMarshaller;
+    
     @Inject
     @Marshals(AssignableTask.class)
     private AssignableTaskMarshaller assignableTaskMarshaller;
+    
     @Inject
     @Marshals(TaskAssignment.class)
     private TaskAssignmentMarshaller taskAssignmentMarshaller;
