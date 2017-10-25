31c0df1b36081df05a3a38311f2ffa57777b4159
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-10-25 18:19:44 +0000

    CCM NG: First part of admin UI for Sites
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5084 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: b93a6484d9de425c865a6151f76fa5137e452a86

---
+++ ccm-core/src/main/java/com/arsdigita/ui/admin/sites/SitesTableModel.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2017 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package com.arsdigita.ui.admin.sites;
+
+import com.arsdigita.bebop.table.TableModel;
+
+import org.libreccm.cdi.utils.CdiUtil;
+import org.libreccm.sites.Site;
+import org.libreccm.sites.SiteRepository;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public class SitesTableModel implements TableModel {
+
+    private final Iterator<Site> iterator;
+    private Site current;
+
+    public SitesTableModel() {
+
+        final CdiUtil cdiUtil = CdiUtil.createCdiUtil();
+        final SiteRepository siteRepo = cdiUtil.findBean(SiteRepository.class);
+        iterator = siteRepo.findAll().iterator();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean nextRow() {
+        if (iterator.hasNext()) {
+            current = iterator.next();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object getElementAt(final int columnIndex) {
+
+        switch (columnIndex) {
+            case SitesTable.COL_SITE_DOMAIN:
+                return current.getDomainOfSite();
+            case SitesTable.COL_IS_DEFAULT_SITE:
+                return current.isDefaultSite();
+            case SitesTable.COL_DEFAULT_THEME:
+                return current.getDefaultTheme();
+            default:
+                throw new IllegalArgumentException("Illegal column index");
+        }
+    }
+
+    @Override
+    public Object getKeyAt(final int columnIndex) {
+        return current.getObjectId();
+    }
+
+}
