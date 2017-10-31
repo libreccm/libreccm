d4d524def8bdd9efb6cb32bc41d9bef9b1b88ca8
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-10-31 19:12:26 +0000

    CCM NG: Various things including a new version of the StaticThemeProvider (not finished yet)
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5093 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/main/java/org/libreccm/theming/StaticThemeProvider.java
+++ ccm-core/src/main/java/org/libreccm/theming/StaticThemeProvider.java
@@ -18,82 +18,181 @@
  */
 package org.libreccm.theming;
 
-import static org.libreccm.theming.ThemeConstants.*;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.libreccm.core.UnexpectedErrorException;
 import org.libreccm.theming.manifest.ThemeManifest;
 import org.libreccm.theming.manifest.ThemeManifestUtil;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
 /**
- * A theme provider implementation which serves themes from the class path
- * ({@code /themes)}. This implementation does not support changes to the
- * theme(s) and files.
  *
  * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
  */
 @RequestScoped
 public class StaticThemeProvider implements ThemeProvider {
 
-    private static final Logger LOGGER = LogManager
-        .getLogger(StaticThemeProvider.class);
+    private static final long serialVersionUID = 7174370298224448067L;
+    private static final Logger LOGGER = LogManager.getLogger(
+        StaticThemeProvider.class);
 
-    /**
-     * Path the the static themes.
-     */
     private static final String THEMES_DIR = "/themes";
-    private static final long serialVersionUID = -8701021965452233811L;
+    private static final String THEME_MANIFEST_JSON = THEMES_DIR
+                                                          + "/%s/theme.json";
+    private static final String THEME_MANIFEST_XML = THEMES_DIR
+                                                         + "/%s/theme.xml";
 
     @Inject
-    private ThemeFileInfoUtil themeFileInfoUtil;
-
-    @Inject
-    private ThemeManifestUtil themeManifests;
+    private ThemeManifestUtil manifestUtil;
 
     @Override
     public List<ThemeInfo> getThemes() {
 
-        LOGGER.debug("Retrieving info about all static themes...");
+        final Reflections reflections = new Reflections(
+            new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("themes"))
+                .setScanners(new ResourcesScanner()));
+        final Set<String> resources = reflections
+            .getResources(Pattern.compile("themes/.*/theme.json"));
+        LOGGER.debug("Found resources:");
+        for (final String resource : resources) {
+            LOGGER.debug("\t{}", resource);
+        }
 
-        try (final FileSystem jarFileSystem = FileSystems.newFileSystem(
-            getJarUri(), Collections.emptyMap())) {
+        final URL themesUrl = StaticThemeProvider.class.getResource(THEMES_DIR);
 
-            final Path themesPath = jarFileSystem.getPath(THEMES_DIR);
-            if (!Files.isDirectory(themesPath)) {
-                LOGGER.warn(THEMES_DIR + " is not a directory. Returning "
-                                + "empty list.");
-                return Collections.emptyList();
+        if (themesUrl == null) {
+            throw new UnexpectedErrorException(
+                "Static themes directory does not"
+                    + "exist in class path. Something is wrong.");
+        }
+
+        File directory;
+        try {
+            directory = new File(themesUrl.toURI());
+        } catch (URISyntaxException ex) {
+            throw new UnexpectedErrorException(ex);
+        } catch (IllegalArgumentException ex) {
+            directory = null;
+        }
+
+        final List<String> themeDirs = new ArrayList<>();
+        if (directory != null && directory.exists()) {
+            final String[] files = directory.list();
+            for (final String file : files) {
+                themeDirs.add(file);
             }
+        } else {
+            final String jarPath = themesUrl
+                .getFile()
+                .replaceFirst("[.]jar[!].*", ".jar")
+                .replaceFirst("file:", "");
 
-            return Files
-                .list(themesPath)
-                .filter(this::isTheme)
-                .map(this::generateThemeInfo)
-                .collect(Collectors.toList());
+            final JarFile jarFile;
+            try {
+                jarFile = new JarFile(jarPath);
+            } catch (IOException ex) {
+                throw new UnexpectedErrorException(ex);
+            }
+            final Enumeration<JarEntry> jarEntries = jarFile.entries();
+            while (jarEntries.hasMoreElements()) {
+                final JarEntry jarEntry = jarEntries.nextElement();
+                final String jarEntryName = jarEntry.getName();
+
+                if (jarEntryName.startsWith("themes")
+                        && jarEntryName.length() > "themes/".length()
+                        && jarEntryName.endsWith("/")) {
+                    themeDirs.add(jarEntryName);
+                }
+            }
+        }
+
+        return themeDirs
+            .stream()
+            .filter(dirPath -> isThemeDir(dirPath))
+            .map(dirPath -> loadThemeManifest(dirPath))
+            .map(manifest -> generateThemeInfo(manifest))
+            .collect(Collectors.toList());
+    }
+
+    private boolean isThemeDir(final String dirPath) {
+
+        Objects.requireNonNull(dirPath);
+
+        final URL manifestJsonUrl = StaticThemeProvider.class.getResource(
+            String.format(THEME_MANIFEST_JSON, dirPath));
+        final URL manifestXmlUrl = StaticThemeProvider.class.getResource(
+            String.format(THEME_MANIFEST_XML, dirPath));
+
+        return (manifestJsonUrl != null) || (manifestXmlUrl != null);
+    }
+
+    private ThemeManifest loadThemeManifest(final String dirPath) {
+
+        Objects.requireNonNull(dirPath);
+
+        final URL manifestJsonUrl = StaticThemeProvider.class.getResource(
+            String.format(THEME_MANIFEST_JSON, dirPath));
+        final URL manifestXmlUrl = StaticThemeProvider.class.getResource(
+            String.format(THEME_MANIFEST_XML, dirPath));
+
+        final URL manifestUrl;
+        if (manifestJsonUrl != null) {
+            manifestUrl = manifestJsonUrl;
+        } else if (manifestXmlUrl != null) {
+            manifestUrl = manifestXmlUrl;
+        } else {
+            throw new IllegalArgumentException(String
+                .format("Path \"%s\" does not point to a valid theme manifest.",
+                        dirPath));
+        }
+
+        final ThemeManifest themeManifest;
+        try (final InputStream inputStream = manifestUrl.openStream()) {
+
+            themeManifest = manifestUtil.loadManifest(inputStream,
+                                                      manifestUrl.toString());
 
         } catch (IOException ex) {
             throw new UnexpectedErrorException(ex);
         }
+
+        return themeManifest;
+    }
+
+    private ThemeInfo generateThemeInfo(final ThemeManifest manifest) {
+
+        Objects.requireNonNull(manifest);
+
+        final ThemeInfo info = new ThemeInfo();
+        info.setManifest(manifest);
+        info.setProvider(getClass());
+        info.setVersion(ThemeVersion.LIVE);
+
+        return info;
     }
 
     @Override
@@ -106,31 +205,13 @@ public class StaticThemeProvider implements ThemeProvider {
                                             final ThemeVersion version) {
 
         Objects.requireNonNull(theme);
-        Objects.requireNonNull(version);
 
-        if (theme.matches("\\s*")) {
-            throw new IllegalArgumentException(
-                "The name of the theme can't be empty.");
+        if (isThemeDir(theme)) {
+            return Optional.of(generateThemeInfo(loadThemeManifest(theme)));
+        } else {
+            return Optional.empty();
         }
 
-        LOGGER.debug("Trying to find static theme \"{}\"...",
-                     theme);
-
-        try (final FileSystem jarFileSystem = FileSystems
-            .newFileSystem(getJarUri(), Collections.emptyMap())) {
-
-            final Path themePath = jarFileSystem
-                .getPath(String.format(THEMES_DIR + "/%s", theme));
-
-            if (isTheme(themePath)) {
-                return Optional.of(generateThemeInfo(themePath));
-            } else {
-                return Optional.empty();
-            }
-
-        } catch (IOException ex) {
-            throw new UnexpectedErrorException(ex);
-        }
     }
 
     @Override
@@ -138,30 +219,9 @@ public class StaticThemeProvider implements ThemeProvider {
                                  final ThemeVersion version) {
 
         Objects.requireNonNull(theme);
-        Objects.requireNonNull(version);
 
-        if (theme.isEmpty() || theme.matches("\\s*")) {
-            throw new IllegalArgumentException(
-                "The name of the theme can't be empty.");
-        }
+        return isThemeDir(theme);
 
-        LOGGER.debug("Determining if there is static theme \"{}\"...",
-                     theme);
-
-        try (final FileSystem jarFileSystem = FileSystems
-            .newFileSystem(getJarUri(), Collections.emptyMap())) {
-
-            final Path themePath = jarFileSystem
-                .getPath(String.format(THEMES_DIR + "/%s", theme));
-
-            LOGGER.debug("Is there a static theme \"{}\": {}",
-                         theme,
-                         isTheme(themePath));
-            return isTheme(themePath);
-
-        } catch (IOException ex) {
-            throw new UnexpectedErrorException(ex);
-        }
     }
 
     @Override
@@ -169,128 +229,19 @@ public class StaticThemeProvider implements ThemeProvider {
                                               final ThemeVersion version,
                                               final String path) {
 
-        Objects.requireNonNull(theme);
-        Objects.requireNonNull(version);
-        Objects.requireNonNull(path);
-
-        if (theme.isEmpty() || theme.matches("\\s*")) {
-            throw new IllegalArgumentException(
-                "The name of the theme can't be empty.");
-        }
-
-        final String pathToDir;
-        if ("".equals(path)) {
-            pathToDir = "/";
-        } else {
-            pathToDir = path;
-        }
-
-        LOGGER.debug("Listing all files in path \"{]\" of theme \"{}\"...",
-                     path,
-                     theme);
-
-        final List<ThemeFileInfo> infos;
-        try (final FileSystem jarFileSystem = FileSystems
-            .newFileSystem(getJarUri(), Collections.emptyMap())) {
-
-            final Path themePath = jarFileSystem
-                .getPath(String.format(THEMES_DIR + "/%s", theme));
-
-            if (!isTheme(themePath)) {
-                throw new IllegalArgumentException(String
-                    .format("Theme \"%s\" does not exist.",
-                            theme));
-            }
-
-            final Path dirPath = themePath.resolve(pathToDir);
-            if (Files.exists(dirPath)) {
-
-                if (Files.isDirectory(dirPath)) {
-
-                    try (final Stream<Path> stream = Files.list(dirPath)) {
-                        infos = stream
-                            .map(themeFileInfoUtil::buildThemeInfo)
-                            .collect(Collectors.toList());
-                    }
-                } else {
-                    infos = new ArrayList<>();
-                    infos.add(themeFileInfoUtil.buildThemeInfo(dirPath));
-                }
-            } else {
-                throw new IllegalArgumentException(String
-                    .format("No file/directory \"%s\" in theme \"%s\".",
-                            path,
-                            theme));
-            }
-
-        } catch (IOException ex) {
-            throw new UnexpectedErrorException(ex);
-        }
-
-        LOGGER.debug("Files in path \"{}\" of static theme \"{}\": {}",
-                     pathToDir,
-                     theme,
-                     Objects.toString(infos));
-        return infos;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Optional<InputStream> getThemeFileAsStream(final String theme,
-                                                      final ThemeVersion version,
-                                                      final String path) {
-        Objects.requireNonNull(theme);
-        Objects.requireNonNull(version);
-        Objects.requireNonNull(path);
-
-        if (theme.isEmpty() || theme.matches("\\s*")) {
-            throw new IllegalArgumentException(
-                "The name of the theme can't be empty.");
-        }
-
-        if (path.isEmpty() || path.matches("\\s*")) {
-            throw new IllegalArgumentException(
-                "The name of the theme can't be empty.");
-        }
-
-        try (final FileSystem jarFileSystem = FileSystems
-            .newFileSystem(getJarUri(), Collections.emptyMap())) {
-
-            final Path themePath = jarFileSystem
-                .getPath(String.format(THEMES_DIR + "/%s", theme));
-
-            final Path filePath;
-            if (path.charAt(0) == '/') {
-                filePath = themePath.resolve(path.substring(1));
-            } else {
-                filePath = themePath.resolve(path);
-            }
-
-            if (!Files.isRegularFile(filePath)) {
-                throw new IllegalArgumentException(String
-                    .format("The provided path \"%s\" in theme \"%s\" points "
-                                + "not to a regular file.",
-                            path,
-                            theme));
-            }
-
-            if (Files.exists(filePath)) {
-                return Optional.of(Files.newInputStream(filePath));
-            } else {
-                return Optional.empty();
-            }
-
-        } catch (IOException ex) {
-            throw new UnexpectedErrorException(ex);
-        }
-
+    public Optional<InputStream> getThemeFileAsStream(String theme,
+                                                      ThemeVersion version,
+                                                      String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public OutputStream getOutputStreamForThemeFile(final String theme,
-                                                    final String path) {
-
-        throw new UnsupportedOperationException("Not supported by this"
-                                                    + " implemetentation");
+    public OutputStream getOutputStreamForThemeFile(String theme, String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -305,72 +256,7 @@ public class StaticThemeProvider implements ThemeProvider {
 
     @Override
     public void publishTheme(final String theme) {
-        LOGGER.info("StaticThemeProvider#publishTheme(String) called, but "
-                        + "StaticThemeProvider does not support draft/live "
-                        + "themes.");
-    }
-
-    private URI getJarUri() {
-
-        LOGGER.debug("Getting URI of JAR...");
-
-        final String themesUrl = getClass().getResource(THEMES_DIR).toString();
-        LOGGER.debug("Full URL of " + THEMES_DIR + " directory: {}", themesUrl);
-
-        final int index = themesUrl.indexOf('!');
-        final String pathToJar = themesUrl.substring(0, index);
-
-        final URI uri = URI.create(pathToJar);
-        LOGGER.debug("URI to JAR is \"%s\".", uri.toString());
-        return uri;
-    }
-
-    private boolean isTheme(final Path path) {
-
-        Objects.requireNonNull(path);
-
-        if (!Files.isDirectory(path)) {
-            return false;
-        }
-
-        final Path manifestPathJson = path.resolve(THEME_MANIFEST_JSON);
-        final Path manifestPathXml = path.resolve(THEME_MANIFEST_XML);
-
-        return Files.exists(manifestPathJson) || Files.exists(manifestPathXml);
-    }
-
-    private ThemeInfo generateThemeInfo(final Path path) {
-
-        Objects.requireNonNull(path);
-
-        if (!Files.exists(path)) {
-            throw new IllegalArgumentException(String
-                .format("The provided path \"%s\" does "
-                            + "not exist.",
-                        path.toString()));
-        }
-
-        final Path manifestPathJson = path.resolve(THEME_MANIFEST_JSON);
-        final Path manifestPathXml = path.resolve(THEME_MANIFEST_XML);
-
-        final ThemeManifest manifest;
-        if (Files.exists(manifestPathJson)) {
-            manifest = themeManifests.loadManifest(manifestPathJson);
-        } else if (Files.exists(manifestPathXml)) {
-            manifest = themeManifests.loadManifest(manifestPathXml);
-        } else {
-            throw new IllegalArgumentException(String
-                .format("The provided path \"%s\" does "
-                            + "contain a theme manifest file.",
-                        path.toString()));
-        }
-
-        final ThemeInfo themeInfo = new ThemeInfo();
-        themeInfo.setVersion(ThemeVersion.LIVE);
-        themeInfo.setProvider(getClass());
-        themeInfo.setManifest(manifest);
-
-        return themeInfo;
+        //No op in this implementation.
     }
 
 }
