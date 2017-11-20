3e99b283a93cfb2d3350e8c6d4f8d6a7cc11afd3
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2017-11-20 18:57:18 +0000

    CCM NG: First part of the Admin UI for PageModels (replacement for JSP based templates)
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5125 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: 5d5d00723c278d27e20e35189fcaac462552c982

---
+++ ccm-core/src/main/java/com/arsdigita/ui/admin/pagemodels/PageModelsTableRow.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2017 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package com.arsdigita.ui.admin.pagemodels;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+class PageModelsTableRow implements Comparable<PageModelsTableRow>,
+                                    Serializable {
+
+    private static final long serialVersionUID = 7497498047332094014L;
+
+    private long modelId;
+
+    private String name;
+
+    private boolean live;
+
+    private String title;
+
+    private String description;
+
+    private String applicationName;
+
+    public long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(final long modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public boolean isLive() {
+        return live;
+    }
+
+    public void setLive(final boolean live) {
+        this.live = live;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(final String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    @Override
+    public int compareTo(final PageModelsTableRow other) {
+
+        int result;
+
+        result = applicationName.compareTo(other.getApplicationName());
+        if (result != 0) {
+            return result;
+        }
+
+        result = name.compareTo(other.getName());
+        if (result != 0) {
+            return result;
+        }
+
+        return title.compareTo(other.getTitle());
+    }
+
+}
