332532e78c3d211c3f171e6a31199c503d0752b3
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-01-19 14:12:43 +0000

    CCM NG: First part of CMS specific modules for the ccm-editor
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5206 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-cms-editor/src/main/typescript/ccm-editor/ccm-cms-editor.ts
@@ -0,0 +1,206 @@
+import { CCMEditor, CCMEditorCommand, CCMEditorCommandType } from "./ccm-editor";
+
+export class InsertInternalLinkCommand extends CCMEditorCommand {
+
+    private button: Element;
+
+    constructor(editor: CCMEditor, settings: any) {
+        super(editor, settings);
+
+        this.button = this.fragment
+            .appendChild(document.createElement("button"));
+
+        const icon: Element = this.button
+            .appendChild(document.createElement("i"));
+        icon.className = "fa fa-link";
+
+        const text: Element = this.button
+            .appendChild(document.createElement("span"));
+        this.button
+            .setAttribute("title", "Insert a link to a ContentItem");
+        text.textContent = "Create internal link";
+        text.className = "ccm-editor-accessibility";
+
+        this.button.addEventListener("click", function(event){
+
+            event.preventDefault();
+
+            const currentRange: Range = document.getSelection().getRangeAt(0);
+
+            const dialogFragment: DocumentFragment = document
+                .createDocumentFragment();
+            const dialogElem: Element = dialogFragment
+                .appendChild(document.createElement("div"));
+            dialogElem.className = "ccm-editor-selectdialog";
+            const dialogTitleElem: Element = dialogElem
+                .appendChild(document.createElement("h1"));
+            dialogTitleElem.textContent = "Insert link to a Content Item";
+
+            const closeButton = dialogElem
+                .appendChild(document.createElement("button"));
+            closeButton.setAttribute("id", "ccm-editor-selectdialog-closebutton");
+            closeButton.textContent = "&#x2715";
+            closeButton.addEventListener("click", function(event){
+                event.preventDefault();
+                const bodyElem = document.getElementsByTagName("body").item(0);
+                bodyElem.removeChild(dialogElem);
+                document.getSelection().removeAllRanges();
+                document.getSelection().addRange(currentRange);
+                return false;
+            });
+
+            const filterForm: Element = dialogElem
+                .appendChild(document.createElement("div"));
+            const contentSectionSelectLabel: Element = filterForm
+                .appendChild(document.createElement("label"));
+            contentSectionSelectLabel
+                .setAttribute("for", "ccm-editor-contentsection-select");
+            contentSectionSelectLabel.textContent = "Show items from Content Section";
+            const contentSectionSelect: HTMLSelectElement = filterForm
+                .appendChild(document.createElement("select"));
+            contentSectionSelect
+                .setAttribute("id", "ccm-editor-contentsection-select");
+            const filterInputLabel: Element = filterForm
+                .appendChild(document.createElement("label"));
+            filterInputLabel.setAttribute("id", "ccm-editor-itemfilter");
+            filterInputLabel.textContent = "Filter items";
+            const filterInput: HTMLInputElement = filterForm
+                .appendChild(document.createElement("input"));
+            filterInput.setAttribute("id", "ccm-editor-itemfilter");
+            filterInput.setAttribute("type", "text");
+            const applyFiltersButton: Element = filterForm
+                .appendChild(document.createElement("button"));
+            applyFiltersButton.textContent = "Clear filters";
+            applyFiltersButton.addEventListener("click", function(event){
+                event.preventDefault();
+                return false;
+            });
+            const clearFiltersButton: Element = filterForm
+                .appendChild(document.createElement("button"));
+            clearFiltersButton.textContent = "Clear filters";
+            clearFiltersButton.addEventListener("click", function(event){
+                event.preventDefault();
+                filterInput.value = "";
+                return false;
+            });
+
+            const table: Element = dialogElem
+                .appendChild(document.createElement("table"));
+            const tableHead: Element = table
+                .appendChild(document.createElement("thead"));
+            const headerRow: Element = tableHead
+                .appendChild(document.createElement("tr"));
+            const titleColHeader: Element = headerRow
+                .appendChild(document.createElement("th"));
+            const typeColHeader: Element = headerRow
+                .appendChild(document.createElement("th"));
+            const placeColHeader: Element = headerRow
+                .appendChild(document.createElement("th"));
+            titleColHeader.textContent = "Title";
+            typeColHeader.textContent = "Type";
+            placeColHeader.textContent = "Place";
+            const tableBody: Element = table
+                .appendChild(document.createElement("tbody"));
+
+            const contextPrefix = editor.getDataAttribute("context-prefix");
+            // Get content sections
+            const currentSection = editor
+                .getDataAttribute("current-contentsection-primaryurl");
+            const sectionsUrl = contextPrefix + "/content-sections/";
+            const sectionsRequest = new XMLHttpRequest();
+            sectionsRequest.open("GET", sectionsUrl);
+            sectionsRequest.withCredentials = true;
+            sectionsRequest.addEventListener("load", function(event){
+                if (sectionsRequest.status >= 200
+                    && sectionsRequest.status <= 300) {
+
+                    const sections = JSON.parse(sectionsRequest.responseText);
+                    for(let i = 0; i < sections.length; ++i) {
+                        const section = sections[i];
+                        const option: Element = contentSectionSelect
+                            .appendChild(document.createElement("option"));
+                        option.setAttribute("value", section["primaryUrl"]);
+                        option.textContent = section["primaryUrl"];
+                        if (section["primaryUrl"] === currentSection) {
+                            option.setAttribute("selected", "selected");
+                        }
+                    }
+                }
+            });
+
+            // Get items
+            let itemsUrl = contextPrefix
+                + "/content-sections/"
+                + contentSectionSelect.value
+                + "/items";
+            if (filterInput.value !== null && filterInput.value.length > 0) {
+                itemsUrl + "?query=" + filterInput.value;
+            }
+            const itemsRequest = new XMLHttpRequest();
+            itemsRequest.open("GET", itemsUrl);
+            itemsRequest.withCredentials = true;
+            itemsRequest.addEventListener("load", function(event){
+                if (itemsRequest.status >= 200 && itemsRequest.status <= 300) {
+
+                    const items = JSON.parse(itemsRequest.responseText);
+                    for(let i = 0; i < items.length; ++i) {
+                        const item = items[i];
+                        const row: Element = tableBody
+                            .appendChild(document.createElement("tr"));
+                        const dataTitle = row
+                            .appendChild(document.createElement("td"));
+                        const dataType = row
+                            .appendChild(document.createElement("td"));
+                        const dataPlace =
+                            row.appendChild(document.createElement("td"));
+
+                        const selectItemButton = dataTitle
+                            .appendChild(document.createElement("button"));
+                        selectItemButton.textContent = item["title"];
+                        selectItemButton
+                            .addEventListener("click", function(event) {
+
+                                event.preventDefault();
+
+                                document.removeChild(dialogElem);
+                                document.getSelection().removeAllRanges();
+                                document.getSelection().addRange(currentRange);
+
+                                document.execCommand("insertLink",
+                                                     false,
+                                                     contextPrefix
+                                                     + "/redirect/?oid="
+                                                     + item["itemId"]);
+
+                                return false;
+                        });
+
+                        dataType.textContent = item["typeLabel"];
+                        dataType.textContent = item["place"];
+                    }
+                }
+            });
+
+            const bodyElem = document.getElementsByTagName("body").item(0);
+            bodyElem.appendChild(dialogFragment);
+
+            return false;
+        });
+    }
+
+    getCommandType(): CCMEditorCommandType {
+        return CCMEditorCommandType.INSERT_INLINE;
+    }
+
+    selectionChanged(selection: Selection) {
+
+    }
+
+    enableCommand(): void {
+        this.button.removeAttribute("disabled");
+    }
+
+    disableCommand(): void {
+        this.button.setAttribute("disabled", "true");
+    }
+}
---
+++ ccm-cms-editor/src/site/markdown/index.md
@@ -0,0 +1,8 @@
+CCM Editor modules for LibreCMS
+---
+
+This module provides two new commands for the CCM Editor which are only useful
+in combination with the LibreCMS module:
+
+* Insert Internal Link: Adds an internal link to a content item
+* Insert Image: Inserts a image from the media library of LibreCMS
