85029d348853682fa9645090f55cbe6be529c0ad
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-01-26 13:48:11 +0000

    CCM NG: Tab Roles in Content Section Admin now works
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5221 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: cac154539c35f7a5532a626392d1e8babbbce4ab

--- ccm-core/src/main/java/org/libreccm/core/AbstractEntityRepository.java
+++ ccm-core/src/main/java/org/libreccm/core/AbstractEntityRepository.java
@@ -31,7 +31,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
