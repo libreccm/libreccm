bed824cde7314b8b0234133fb2ae10920bf01f2e
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-03-07 18:23:53 +0000

    CCM NG: Removed duplicate line from SQL scripts for tests in ccm-cms
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5330 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-core/src/main/java/org/libreccm/workflow/AssignableTask.java
+++ ccm-core/src/main/java/org/libreccm/workflow/AssignableTask.java
@@ -136,7 +136,7 @@ public class AssignableTask extends Task implements Serializable, Portable {
     /**
      * The roles to which task is assigned.
      */
-    @OneToMany(mappedBy = "task", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "task")
     @JsonIgnore
     private List<TaskAssignment> assignments;
 
