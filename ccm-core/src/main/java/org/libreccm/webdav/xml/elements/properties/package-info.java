f718b32713eecd822ce4b30f228bbeea7f3468fd
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-04-16 18:12:33 +0000

    CCM NG: More classes for WebDAV support
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5375 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-core/src/main/java/org/libreccm/webdav/xml/elements/properties/package-info.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+/**
+ * This package contains WebDAV Properties.
+ *
+ * The classes in this package  are based on a class/interface from java.net WebDAV Project:
+ *
+ * <a href="https://gitlab.com/headcrashing/webdav-jaxrs/blob/master/src/main/java/net/java/dev/webdav/jaxrs/Headers.java">https://gitlab.com/headcrashing/webdav-jaxrs/blob/master/src/main/java/net/java/dev/webdav/jaxrs/Headers.java</a>
+ *
+ *
+ * @author Markus KARG (mkarg@java.net)
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ *
+ * @see
+ * <a href="http://www.webdav.org/specs/rfc4918.html#dav.properties">Chapter 15
+ * "DAV Properties" of RFC 4918 "HTTP Extensions for Web Distributed Authoring
+ * and Versioning (WebDAV)"</a>
+ */
+@XmlSchema(namespace = "DAV:",
+           xmlns = @XmlNs(prefix = "D", namespaceURI = "DAV:"),
+           elementFormDefault = QUALIFIED)
+package org.libreccm.webdav.xml.elements.properties;
+
+import static javax.xml.bind.annotation.XmlNsForm.QUALIFIED;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
