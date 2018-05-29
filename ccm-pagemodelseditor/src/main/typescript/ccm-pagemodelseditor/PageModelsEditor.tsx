400c4424850dc5fc49da8bbc83970e3d018d8816
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-05-29 12:50:51 +0000

    CCM NG: Basic integration of NPM modules and Maven build system
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5461 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-pagemodelseditor/src/main/typescript/ccm-pagemodelseditor/PageModelsEditor.tsx
+++ ccm-pagemodelseditor/src/main/typescript/ccm-pagemodelseditor/PageModelsEditor.tsx
@@ -2,9 +2,44 @@ import * as React from "react";
 
 export { PageModelEditor };
 
+/**
+ * To render the PageModelEditor create a Typescript file with the following
+ * minimal content:
+ *
+ * import * as React from "react";
+ * import { render } from "react-dom";
+ *
+ * import { PageModelEditor } from "./PageModelsEditor";
+ *
+ * render(
+ *     <PageModelEditor />,
+ *     document.getElementById("cms-content"),
+ * );
+ */
+
 class PageModelEditor extends React.Component<{}, {}> {
 
+    private getCcmApplication(): string {
+
+        const dataElem: HTMLElement | null = document
+            .querySelector("#page-models-editor.react-data");
+
+        if (dataElem === null) {
+            return "???";
+        } else {
+            const value: string | null = dataElem.getAttribute("data-ccm-application");
+            if (value === null) {
+                return "???";
+            } else {
+                return value;
+            }
+        }
+    }
+
     public render() {
+
+
+
         return <React.Fragment>
             <div id="left">
                 <div className="column-head"></div>
@@ -18,7 +53,7 @@ class PageModelEditor extends React.Component<{}, {}> {
                 <div className="column-content">
                     PageModelEditor Placeholder
                     <pre>
-                        {document.querySelector("#page-models-editor.react-data").getAttribute("data-ccm-application")}
+                        {this.getCcmApplication()}
                     </pre>
                 </div>
             </div>
