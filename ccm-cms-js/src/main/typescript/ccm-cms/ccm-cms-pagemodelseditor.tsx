d9cbfa4b9e4876f7ebe9b220f4f294cd1577ce12
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2018-05-29 06:25:08 +0000

    CCM NG: Integration of Node/NPM modules into build
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5460 8810af33-2d31-482b-a856-94f89814c4df

---
+++ ccm-cms-js/src/main/typescript/ccm-cms/ccm-cms-pagemodelseditor.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import { PageModelEditor } from "../ccm-pagemodelseditor/PageModelsEditor";
+
+render(
+    <PageModelEditor />,
+    document.getElementById("cms-content"),
+);
+
