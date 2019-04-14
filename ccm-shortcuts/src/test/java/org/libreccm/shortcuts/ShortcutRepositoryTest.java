3d8402effefb4336aa80b0d89bf30fe8c535d1e1
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2019-04-14 14:56:51 +0000

    CCM NG: Fixed dependencies
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@5944 8810af33-2d31-482b-a856-94f89814c4df

--- ccm-shortcuts/src/test/java/org/libreccm/shortcuts/ShortcutRepositoryTest.java
+++ ccm-shortcuts/src/test/java/org/libreccm/shortcuts/ShortcutRepositoryTest.java
@@ -109,6 +109,7 @@ public class ShortcutRepositoryTest {
             .addPackage(org.libreccm.workflow.Workflow.class.getPackage())
             .addPackage(org.libreccm.tests.categories.IntegrationTest.class
                 .getPackage())
+            .addClass(org.libreccm.imexport.Exportable.class)
             .addClass(org.libreccm.shortcuts.Shortcut.class)
             .addClass(org.libreccm.shortcuts.ShortcutRepository.class)
             .addAsLibraries(getModuleDependencies())
