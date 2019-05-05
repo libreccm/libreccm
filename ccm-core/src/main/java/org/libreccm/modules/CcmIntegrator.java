75869de820e1337db0d9e6a13f16ae6feab3186f
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2019-05-05 15:44:15 +0000

    CCM NG: Updated Flyway
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@6012 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: e552cdff5fcb41a97111182d299d1b0c42f9aad9

--- ccm-core/src/main/java/org/libreccm/modules/CcmIntegrator.java
+++ ccm-core/src/main/java/org/libreccm/modules/CcmIntegrator.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationInfo;
-import org.flywaydb.core.internal.util.jdbc.JdbcUtils;
+import org.flywaydb.core.internal.jdbc.JdbcUtils;
 import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.integrator.spi.Integrator;
