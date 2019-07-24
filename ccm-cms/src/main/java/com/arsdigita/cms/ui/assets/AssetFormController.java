b85d4395109b3fd581dacb3b3a22ab1817d85fbf
jensp <jensp@8810af33-2d31-482b-a856-94f89814c4df>
2019-07-24 11:05:04 +0000

    CCM NG: Asset editing current status
    git-svn-id: https://svn.libreccm.org/ccm/ccm_ng@6150 8810af33-2d31-482b-a856-94f89814c4df
    
    Former-commit-id: 944ebafc046fdf631f75e8b686df4346b3931ca6

--- ccm-cms/src/main/java/com/arsdigita/cms/ui/assets/AssetFormController.java
+++ ccm-cms/src/main/java/com/arsdigita/cms/ui/assets/AssetFormController.java
@@ -18,17 +18,65 @@
  */
 package com.arsdigita.cms.ui.assets;
 
-import javax.enterprise.context.RequestScoped;
+import org.librecms.contentsection.Asset;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  *
- *  Controller for the asset forms
+ * Interface for the CDI backend for the forms to manage assets.To avoid
+ * problems with transactions etc.
+ *
+ * the Bebop based forms should access any other CDI beans beside the
+ * approbriate implementation of this interface. To minimize the efford to
+ * create an implementation the {@link AbstractAssetFormController} class should
+ * be used. This class provides basic implementations for most methods.
  * 
+ * Implementations of the methods defined by this interface should annotated with
+ * {@code @Transactional(Transactional.TxType.REQUIRED)}.
+ *
  * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ *
+ * @param <T> The type asset managed by this controller.
  */
-@RequestScoped
-class AssetFormController {
+public interface AssetFormController<T extends Asset> {
+
+    /**
+     * Gets the data for the forms from the asset.
+     *
+     * @param asset          The asset.
+     * @param selectedLocale The selected locale
+     *
+     * @return The values of the properties of the asset for the the selected
+     *         locale.
+     */
+    Map<String, Object> getData(T asset, Locale selectedLocale);
+
+    /**
+     * Updates the asset with the provided data and saves the changes.
+     * 
+     * @param asset
+     * @param selectedLocale
+     * @param data 
+     */
+    void setData(T asset, Locale selectedLocale, Map<String, Object> data);
     
+    /**
+     * Determines in which locales the provided asset is available.
+     * 
+     * @param asset The asset.
+     * @return A list of the locales for which the asset has data.
+     */
+    List<Locale> availableLocales(T asset);
     
-    
+    /**
+     * Determines for which the provided asset has no data. 
+     * 
+     * @param asset The asset.
+     * @return A list of the locales for which the asset has data yet.
+     */
+    List<Locale> creatableLocales(T asset);
+
 }
