c909a644965716c9fa12ece4b291e9cf0c68fefd
Jens Pelzetter <jens@jp-digital.de>
2019-11-23 19:21:16 +0100

    ProfileItem

---
+++ ccm-cms-profile/src/main/java/org/librecms/profilesite/ProfileSiteConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.librecms.profilesite;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public class ProfileSiteConstants {
+    
+    public static final String DB_SCHEMA = "cms_profile";
+    
+    private ProfileSiteConstants() {
+        // Nothing
+    }
+    
+}
