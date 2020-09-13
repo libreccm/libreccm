1b71def16dd28ea1c1e557eca80adad3e142e6b7
Jens Pelzetter <jens@jp-digital.de>
2020-09-13 16:50:49 +0200

    Integration of EE MVC

---
+++ ccm-wildfly/src/main/java/org/libreccm/wildfly/LibreCcmWildfly.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.libreccm.wildfly;
+
+/**
+ * Does nothing, only reuquired to get the Maven JAR working.
+ * 
+ * To be removed when other classes are added to this module.
+ * 
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public class LibreCcmWildfly {
+    
+}
