db19646e4966aa9f3c800815aa9da184cfe6d526
Jens Pelzetter <jens@jp-digital.de>
2020-09-25 20:03:33 +0200

    Forms for managing users (not tested yet!)
    Former-commit-id: 1ecaac58a5f320a8b399a1f84177cf1c4959c22f

--- ccm-core/src/main/java/org/libreccm/ui/admin/usersgroupsroles/EmailFormController.java
+++ ccm-core/src/main/java/org/libreccm/ui/admin/usersgroupsroles/EmailFormController.java
@@ -21,6 +21,7 @@ package org.libreccm.ui.admin.usersgroupsroles;
 import org.libreccm.api.Identifier;
 import org.libreccm.api.IdentifierParser;
 import org.libreccm.core.CoreConstants;
+import org.libreccm.core.EmailAddress;
 import org.libreccm.security.AuthorizationRequired;
 import org.libreccm.security.RequiresPrivilege;
 import org.libreccm.security.User;
@@ -35,15 +36,16 @@ import java.util.Optional;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.mvc.Controller;
-import javax.mvc.MvcContext;
+import javax.mvc.Models;
+import javax.mvc.binding.MvcBinding;
 import javax.transaction.Transactional;
-import javax.ws.rs.DefaultValue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 
 /**
  *
@@ -51,17 +53,20 @@ import javax.ws.rs.QueryParam;
  */
 @RequestScoped
 @Controller
-@Path("/users-groups-roles/users")
-public class UsersController {
+@Path("/users-groups-roles/users/{userIdentifier}/email-addresses")
+public class EmailFormController {
 
     @Inject
     private AdminMessages adminMessages;
 
     @Inject
+    private EmailFormModel emailFormModel;
+
+    @Inject
     private IdentifierParser identifierParser;
 
     @Inject
-    private MvcContext mvc;
+    private Models models;
 
     @Inject
     private UserDetailsModel userDetailsModel;
@@ -69,80 +74,35 @@ public class UsersController {
     @Inject
     private UserRepository userRepository;
 
-    @Inject
-    private UsersTableModel usersTableModel;
+    @MvcBinding
+    @FormParam("address")
+    @NotBlank
+    @Email
+    private String address;
 
-    @GET
-    @Path("/")
-    @AuthorizationRequired
-    @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    public String getUsers(
-        @QueryParam("filterterm") @DefaultValue("") final String filterTerm
-    ) {
-        usersTableModel.setFilterTerm(filterTerm);
-        return "org/libreccm/ui/admin/users-groups-roles/users.xhtml";
-    }
+    @FormParam("bouncing")
+    private boolean bouncing;
 
-    @GET
-    @Path("/{userIdentifier}/details")
-    @AuthorizationRequired
-    @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    @Transactional(Transactional.TxType.REQUIRED)
-    public String getUserDetails(
-        @PathParam("userIdentifier") final String userIdentifierParam
-    ) {
-        final Identifier identifier = identifierParser.parseIdentifier(
-            userIdentifierParam
-        );
-        final Optional<User> result;
-        switch (identifier.getType()) {
-            case ID:
-                result = userRepository.findById(
-                    Long.parseLong(identifier.getIdentifier())
-                );
-                break;
-            case UUID:
-                result = userRepository.findByUuid(
-                    identifier.getIdentifier()
-                );
-                break;
-            default:
-                result = userRepository.findByName(identifier.getIdentifier());
-                break;
-        }
-
-        if (result.isPresent()) {
-            userDetailsModel.setUser(result.get());
-            return "org/libreccm/ui/admin/users-groups-roles/user-details.xhtml";
-        } else {
-            userDetailsModel.addMessage(
-                new Message(
-                    adminMessages.getMessage(
-                        "usersgroupsroles.users.not_found.message",
-                        Arrays.asList(userIdentifierParam)
-                    ),
-                    MessageType.WARNING
-                )
-            );
-            return "org/libreccm/ui/admin/users-groups-roles/user-not-found.xhtml";
-        }
-    }
+    @FormParam("verified")
+    private boolean verified;
 
     @GET
     @Path("/new")
     @AuthorizationRequired
     @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    public String newUser(
-        @PathParam("userIdentifier") final String userIdentifier
+    @Transactional(Transactional.TxType.REQUIRED)
+    public String getNewEmailAddressForm(
+        @PathParam("userIdentifier") final String userIdentifierParam
     ) {
-        return "org/libreccm/ui/admin/users-groups-roles/user-form.xhtml";
+        return "org/libreccm/ui/admin/users-groups-roles/email-form.xhtml";
     }
 
-    @GET
-    @Path("/{userIdentifier}/edit")
+    @POST
+    @Path("/new")
     @AuthorizationRequired
     @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    public String editUser(
+    @Transactional(Transactional.TxType.REQUIRED)
+    public String addNewEmailAddress(
         @PathParam("userIdentifier") final String userIdentifierParam
     ) {
         final Identifier identifier = identifierParser.parseIdentifier(
@@ -166,56 +126,19 @@ public class UsersController {
         }
 
         if (result.isPresent()) {
-            userDetailsModel.setUser(result.get());
-            return "org/libreccm/ui/admin/users-groups-roles/user-form.xhtml";
-        } else {
-            userDetailsModel.addMessage(
-                new Message(
-                    adminMessages.getMessage(
-                        "usersgroupsroles.users.not_found.message",
-                        Arrays.asList(userIdentifierParam)
-                    ),
-                    MessageType.WARNING
-                )
-            );
-            return "org/libreccm/ui/admin/users-groups-roles/user-not-found.xhtml";
-        }
-    }
-
-    @POST
-    @Path("/{userIdentifier}/disable")
-    @AuthorizationRequired
-    @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    public String disableUser(
-        @PathParam("userIdentifier") final String userIdentifierParam,
-        @FormParam("confirmed") final boolean confirmed
-    ) {
-        final Identifier identifier = identifierParser.parseIdentifier(
-            userIdentifierParam
-        );
-        final Optional<User> result;
-        switch (identifier.getType()) {
-            case ID:
-                result = userRepository.findById(
-                    Long.parseLong(identifier.getIdentifier())
-                );
-                break;
-            case UUID:
-                result = userRepository.findByUuid(
-                    identifier.getIdentifier()
-                );
-                break;
-            default:
-                result = userRepository.findByName(identifier.getIdentifier());
-                break;
-        }
-
-        if (result.isPresent()) {
             final User user = result.get();
-            user.setBanned(true);
+
+            final EmailAddress emailAddress = new EmailAddress();
+            emailAddress.setAddress(address);
+            emailAddress.setBouncing(bouncing);
+            emailAddress.setVerified(verified);
+            user.addEmailAddress(emailAddress);
+
             userRepository.save(user);
+
             return String.format(
-                "redirect:%s", mvc.uri("UsersController#getUsers")
+                "redirect:/users-groups-roles/users/%s/details",
+                user.getName()
             );
         } else {
             userDetailsModel.addMessage(
@@ -231,28 +154,15 @@ public class UsersController {
         }
     }
 
-    @POST
-    @Path("/{userIdentifier}/email-addresses/{emailId}/remove")
+    @GET
+    @Path("/{emailId}")
     @AuthorizationRequired
     @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
-    public String removeEmailAddress(
+    @Transactional(Transactional.TxType.REQUIRED)
+    public String getEditEmailAddressForm(
         @PathParam("userIdentifier") final String userIdentifierParam,
-        @PathParam("emailId") final int emailId,
-        @FormParam("confirmed") final boolean confirmed
+        @PathParam("emailId") final int emailId
     ) {
-        if (!confirmed) {
-            return String.format(
-                "redirect:%s",
-                mvc.uri(
-                    String.format(
-                        "UsersController#getUserDetails",
-                        "{ userIdentifier: %s}",
-                        userIdentifierParam
-                    )
-                )
-            );
-        }
-
         final Identifier identifier = identifierParser.parseIdentifier(
             userIdentifierParam
         );
@@ -275,30 +185,20 @@ public class UsersController {
 
         if (result.isPresent()) {
             final User user = result.get();
+
             if (user.getEmailAddresses().size() <= emailId) {
-                return String.format(
-                    "redirect:%s",
-                    mvc.uri(
-                        String.format(
-                            "UsersController#getUserDetails",
-                            "{ userIdentifier: %s}",
-                            userIdentifierParam
-                        )
-                    )
-                );
+                models.put("error.userIdentifier", userIdentifierParam);
+                models.put("error.emailId", emailId);
+                return "org/libreccm/ui/admin/users-groups-roles/email-not-found.xhtml";
+            } else {
+                final EmailAddress emailAddress = user
+                    .getEmailAddresses()
+                    .get(emailId);
+                emailFormModel.setAddress(emailAddress.getAddress());
+                emailFormModel.setBouncing(emailAddress.isBouncing());
+                emailFormModel.setVerified(emailAddress.isVerified());
+                return "org/libreccm/ui/admin/users-groups-roles/email-form.xhtml";
             }
-            user.getEmailAddresses().remove(emailId);
-            userRepository.save(user);
-            return String.format(
-                "redirect:%s",
-                mvc.uri(
-                    String.format(
-                        "UsersController#getUserDetails",
-                        "{ userIdentifier: %s}",
-                        userIdentifierParam
-                    )
-                )
-            );
         } else {
             userDetailsModel.addMessage(
                 new Message(
@@ -309,6 +209,71 @@ public class UsersController {
                     MessageType.WARNING
                 )
             );
+
+            return "org/libreccm/ui/admin/users-groups-roles/user-not-found.xhtml";
+        }
+    }
+    
+    @POST
+    @Path("/{emailId}")
+    @AuthorizationRequired
+    @RequiresPrivilege(CoreConstants.PRIVILEGE_ADMIN)
+    @Transactional(Transactional.TxType.REQUIRED)
+    public String updateEmailAddress(
+        @PathParam("userIdentifier") final String userIdentifierParam,
+        @PathParam("emailId") final int emailId
+    ) {
+        final Identifier identifier = identifierParser.parseIdentifier(
+            userIdentifierParam
+        );
+        final Optional<User> result;
+        switch (identifier.getType()) {
+            case ID:
+                result = userRepository.findById(
+                    Long.parseLong(identifier.getIdentifier())
+                );
+                break;
+            case UUID:
+                result = userRepository.findByUuid(
+                    identifier.getIdentifier()
+                );
+                break;
+            default:
+                result = userRepository.findByName(identifier.getIdentifier());
+                break;
+        }
+
+        if (result.isPresent()) {
+            final User user = result.get();
+
+            if (user.getEmailAddresses().size() <= emailId) {
+                models.put("error.userIdentifier", userIdentifierParam);
+                models.put("error.emailId", emailId);
+                return "org/libreccm/ui/admin/users-groups-roles/email-not-found.xhtml";
+            } else {
+                final EmailAddress emailAddress = user
+                    .getEmailAddresses()
+                    .get(emailId);
+                
+                emailAddress.setAddress(address);
+                emailAddress.setBouncing(bouncing);
+                emailAddress.setVerified(verified);
+                
+                userRepository.save(user);
+                
+                return "org/libreccm/ui/admin/users-groups-roles/email-form.xhtml";
+            }
+        } else {
+            userDetailsModel.addMessage(
+                new Message(
+                    adminMessages.getMessage(
+                        "usersgroupsroles.users.not_found.message",
+                        Arrays.asList(userIdentifierParam)
+                    ),
+                    MessageType.WARNING
+                )
+            );
+
             return "org/libreccm/ui/admin/users-groups-roles/user-not-found.xhtml";
         }
     }
