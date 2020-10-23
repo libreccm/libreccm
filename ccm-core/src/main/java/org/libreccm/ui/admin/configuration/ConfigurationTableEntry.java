48c2faff70c26801458076fe54da45e766a32449
Jens Pelzetter <jens@jp-digital.de>
2020-10-23 18:29:17 +0200

    Configurations list

---
+++ ccm-core/src/main/java/org/libreccm/ui/admin/configuration/ConfigurationTableEntry.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2020 LibreCCM Foundation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package org.libreccm.ui.admin.configuration;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public class ConfigurationTableEntry
+    implements Comparable<ConfigurationTableEntry> {
+
+    private String name;
+
+    private String title;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int compareTo(final ConfigurationTableEntry other) {
+        int result = Objects.compare(
+            title,
+            Objects.requireNonNull(other).getTitle(),
+            String::compareTo
+        );
+        if (result == 0) {
+            result = Objects.compare(
+                name,
+                Objects.requireNonNull(other).getName(),
+                String::compareTo
+            );
+        }
+        return result;
+    }
+
+}
