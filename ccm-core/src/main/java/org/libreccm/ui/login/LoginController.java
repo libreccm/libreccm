4a5b38f42e26668cee7eb4a3b4951d56933458b5
Jens Pelzetter <jens@jp-digital.de>
2021-01-06 17:13:40 +0100

    Logout Application

--- ccm-core/src/main/java/org/libreccm/ui/login/LoginController.java
+++ ccm-core/src/main/java/org/libreccm/ui/login/LoginController.java
@@ -118,7 +118,7 @@ public class LoginController {
             models.put("loginFailed", true);
             return getLoginForm(uriInfo, returnUrl);
         }
-
+        
         try {
             return Response.seeOther(
                 new URI(
